1a23b8db8e35f8aa90327ac5d32ce18ca88b3297
stik101 <aadilbinfalaque123@gmail.com>
2026-08-25 11:25:12 +0530

    Added the Godot project files into repo lesgoooooo

---
+++ hello.gd
@@ -0,0 +1,73 @@
+extends Control
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+var dragging := false
+var drag_offset := Vector2.ZERO # The initial position of mouse while clicked on sprite
+
+
+func _ready() -> void:
+	sprite.visible = true
+	set_passthrough()
+
+
+func set_passthrough() -> void:
+	
+	# 1. trying to set the polygon
+	var window_size := DisplayServer.window_get_size()
+
+	var rect := sprite.get_rect()
+	var top_left := sprite.to_global(rect.position)
+	var bottom_right := sprite.to_global(rect.end)
+
+	var viewport_size := get_viewport_rect().size
+
+	var scale := Vector2(
+		float(window_size.x) / viewport_size.x,
+		float(window_size.y) / viewport_size.y
+	)
+
+	top_left *= scale
+	bottom_right *= scale
+
+	var polygon := PackedVector2Array([
+		top_left,
+		Vector2(bottom_right.x, top_left.y),
+		bottom_right,
+		Vector2(top_left.x, bottom_right.y)
+	])
+	DisplayServer.window_set_mouse_passthrough(polygon)
+
+
+# THIS IS FOR DRAGGIN SPRITE, WORKS LIKE NORMAL DRAGGING BTW
+func _input(event: InputEvent) -> void:
+	# Mouse button pressed/released
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				# Check whether the click is on the sprite.
+				var local_mouse := sprite.to_local(event.position) # position of mouse wrt sprite
+				# checks if the point is in the sprite area
+				if sprite.get_rect().has_point(local_mouse):
+					dragging = true
+					# DRAG OFFSET ASSIGNED HERE
+					drag_offset = sprite.position - event.position
+					# Disable passthrough while dragging.
+					# EMPYTY PACKEDVECOTR ARRAY DISABLES PASSTHROUGH
+					DisplayServer.window_set_mouse_passthrough(PackedVector2Array())
+					get_viewport().set_input_as_handled()
+			else:
+				# DISABLES DRAGGING IF NOT BEING PRESSED
+				if dragging:
+					dragging = false
+					# Put click-through back.
+					set_passthrough()
+
+	# Mouse movement while dragging
+	elif event is InputEventMouseMotion:
+		if dragging:
+			sprite.position = event.position + drag_offset
+
+
+func _on_button_pressed() -> void:
+	print("pressed!")
---
+++ script/pokedex.gd
@@ -0,0 +1,247 @@
+extends Control
+
+'''
+ID 
+Name
+IMAGES
+TYPE
+BASE STATS [HP, Attack, Defense, Special Attack, Special Defense, and Speed]
+MOVES
+ATTACKS
+DESCRIPTION
+'''
+
+# ALL HTTPREQUEST NODES GO HERE
+@onready var api:HTTPRequest = $PokeAPI/APIName
+@onready var api_image: HTTPRequest = $PokeAPI/APIImage
+@onready var api_cry: HTTPRequest = $PokeAPI/APICry
+@onready var api_entry: HTTPRequest = $PokeAPI/APIEntry
+
+# SEARCH BAR STUFF GO HERE
+@onready var search_bar:TextEdit = $TextEdit
+@onready var search_button:Button = $Button
+@onready var output: Label = $Output
+@onready var poke_cry: AudioStreamPlayer = $PokeCry
+
+# MINI DEX STUFF GO HERE
+@export var dex_anim:AnimationPlayer
+@onready var dex_sprites:Node2D = $DemoDex/DexSprites
+@export var poke_image: TextureRect
+var is_open:bool = false
+@export var closed_sprite:Sprite2D
+@export var open_sprite:Sprite2D
+var current_sprite:Sprite2D
+var is_hovering:bool = false
+var is_waiting:bool = true
+
+@export var entry_text_label:Label
+
+# Pokemon Cry SFX
+var cry_audio:AudioStreamOggVorbis = AudioStreamOggVorbis.new()
+
+# NOTE: POKE ID IS REQUIRED FOR A LOTA STUFF, SO ITS DECLARED EARLIER HERE
+var p_id:int
+
+# API LINKS GO HERE
+var api_link:String = "https://pokeapi.co/api/v2/pokemon/"
+var image_link:String = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+var cry_link:String = 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/'
+var cry_link_legacy:String = 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/'
+var entry_link:String = "https://pokeapi.co/api/v2/pokemon-species/"
+
+# Connect a lota signals (mostly of APIs)
+func _ready() -> void:
+	current_sprite = closed_sprite
+	api.request_completed.connect(_on_request_pokedata)
+	search_button.pressed.connect(search)
+	api_image.request_completed.connect(_on_request_pokeimage)
+	api_cry.request_completed.connect(_on_request_pokecry)
+	api_entry.request_completed.connect(_on_request_pokeentry)
+	print("=====================")
+	print("API IMAGE: ", api_image)
+	print("PROCESS MODE: ", api_image.process_mode)
+	print("INSIDE TREE: ", api_image.is_inside_tree())
+	print("=========================")
+
+# For search bar field
+func search() -> void:
+	var text:String = search_bar.text.to_lower()
+	print("FETCHING DATA (Where's my farfetch'd?)")
+	var error = api.request(api_link + text)
+	
+	if error != OK:
+		print("Oops, there's an error when data was recieved!", str(error))
+	
+# NOTE: ALL OTHER API FUNCTION CALLS ARE DONE AT THE END OF THS FUNC TOO
+func _on_request_pokedata(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
+	print("Request done! Status: ", response_code)
+	var json = JSON.new()
+	var error = json.parse(body.get_string_from_utf8())
+	
+	if error != OK:
+		print("OOPS! There's an error in parsing json")
+		return
+	
+	var data:Dictionary = json.data
+	
+	# COLLECTING ALL DATAS OF POKEMON HERE ON
+	#Name
+	var p_name:String = data["name"]
+	
+	#ID
+	p_id = data["id"]
+	
+	# HEIGHT
+	var p_height:float = data["height"]
+	
+	# WEIGHT
+	var p_weight:float = data['weight']
+	
+	# TYPES
+	var p_type:Array[String] = []
+	for t in data['types']:
+		p_type.append(t['type']['name'])
+	
+	# MOVES
+	var p_moves:Array[String] = []
+	for m in data["moves"]:
+		p_moves.append(m["move"]["name"])
+	
+	# ABILITIES
+	var p_abilities:Array[String] = []
+	for a in data["abilities"]:
+		p_abilities.append(a["ability"]["name"])
+	
+	# BASE STATS!!!
+	var p_hp:float = data["stats"][0]['base_stat'] # HP
+	var p_attack:float = data["stats"][1]['base_stat'] # ATTACK
+	var p_defence:float = data["stats"][2]['base_stat'] # DEFENCE
+	var p_sattack:float = data["stats"][3]['base_stat'] # SPECIAL ATTACK
+	var p_sdefence:float = data["stats"][4]['base_stat'] # SPECIAL DEFENCE
+	var p_speed:float = data["stats"][5]['base_stat'] # SPEED
+	
+	output.text = str(int(data["id"])) + ". " + str(data["name"]).to_upper()
+	print(int(data["id"]),'. ', data["name"])
+	print("Height: ", str(p_height), " Weight: ", str(p_weight))
+	print("Type: ", p_type)
+	print("Moves: ", p_moves)
+	print("\nAbilities: ", p_abilities)
+	print("\nBASE STATS\n")
+	print("HP: ", p_hp)
+	print("Atk: ", p_attack)
+	print("DEF: ", p_defence)
+	print("S. ATK: ", p_sattack)
+	print("S. DEF: ", p_sdefence)
+	print("SPD: ", p_speed)
+	
+	search_image() # since now weve id, we can search the pokemon image
+	search_cry()
+	search_entry()
+
+func search_image() -> void:
+	var error = api_image.request(image_link + str(p_id) + '.png')
+
+func search_cry() -> void:
+	var error
+	if not (p_id == 25 or p_id == 133):
+		error = api_cry.request(cry_link + str(p_id) + '.ogg')
+	else:
+		error = api_cry.request(cry_link_legacy + str(p_id) + '.ogg')
+	if error != OK:
+		print("Oops, there's an error when data was recieved!", str(error))
+
+func search_entry() -> void:
+	var error = api_entry.request(entry_link + str(p_id))
+
+func _on_request_pokeimage(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
+	if response_code != 200:
+		print("Failed to download image for ID: ", p_id)
+		return
+
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	
+	if error == OK:
+		poke_image.texture = ImageTexture.create_from_image(image)
+		print("Pokemon image loaded successfully! ")
+
+func _on_request_pokecry(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
+	if response_code != 200:
+		print("Failed to download cry for ID: ", p_id)
+		return
+	
+	
+	cry_audio = AudioStreamOggVorbis.load_from_buffer(body)
+	poke_cry.stream = cry_audio
+
+func _on_request_pokeentry(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
+	print("\nDESCRIPTION RECIEVED SUCCESFULLY")
+	var json = JSON.new()
+	var error = json.parse(body.get_string_from_utf8())
+	
+	if error != OK:
+		print("OOPS! There's an error in parsing json")
+		return
+	
+	var data:Dictionary = json.data
+	
+	var p_entry:String = ""
+	
+	for e in data["flavor_text_entries"]:
+		if p_entry == '':
+			if SettingsGlobal.language == "en":
+				if e["language"]["name"] == 'en':
+					p_entry = e["flavor_text"]
+	
+	p_entry = p_entry.replace("\n", " ").replace("\f", " ")
+	entry_text_label.text = p_entry
+	
+	print("\n", JSON.stringify(p_entry))
+func _process(delta: float) -> void:
+	# HANDLES THE HOVER POKEDEX FEATURE
+	if not is_open:
+		if closed_sprite.get_rect().has_point(closed_sprite.to_local(get_global_mouse_position())):
+			dex_sprites.scale = Vector2(1.1, 1.1)
+		else:
+			dex_sprites.scale = Vector2(1, 1)
+	else:
+		if entry_text_label.text != '':
+			# HANDLES THE MARQUEE EFFECT IN ENTRLY LABEL
+			var x_limit = entry_text_label.size.x
+			var scroll_speed = 1
+			# Start of effect
+			if is_waiting:
+				await get_tree().create_timer(1).timeout 
+				await get_tree().create_tween().tween_property(entry_text_label, "modulate:a", 1.0, 0.0).finished
+				await get_tree().create_timer(1).timeout
+				is_waiting = false
+			else:
+				if entry_text_label.position.x >= -x_limit:
+					entry_text_label.position.x -= scroll_speed
+				else:
+					is_waiting = true
+					entry_text_label.position.x = 0
+					entry_text_label.modulate.a = 0.0
+		
+# To flip open the pokedex
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				# Check whether the click is on the sprite.
+				var local_mouse := current_sprite.to_local(event.position) # position of mouse wrt sprite
+				# checks if the point is in the sprite area
+				if current_sprite.get_rect().has_point(local_mouse):
+					if is_open:
+						current_sprite = open_sprite
+						dex_anim.play("closed")
+						is_open = false
+					else:
+						current_sprite = closed_sprite
+						dex_anim.play("open")
+						is_open = true
+
+
+
+func _on_cry_button_pressed() -> void:
+	poke_cry.play()
