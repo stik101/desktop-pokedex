c83f60ef9eacc4eb493eed39c95b0d6db52fbf38
stik101 <aadilbinfalaque123@gmail.com>
2026-08-30 01:03:01 +0530

    Added a flip button, shows GENERA of pokemon, and shows the name and ID on a seperate area

--- script/pokedex.gd
+++ script/pokedex.gd
@@ -3,14 +3,14 @@ extends Control
 
 # ALL HTTPREQUEST NODES GO HERE
 @onready var api:HTTPRequest = $PokeAPI/APIName
-@onready var api_image: HTTPRequest = $PokeAPI/APIImage
+@onready var api_image_front: HTTPRequest = $PokeAPI/APIImageFront
+@onready var api_image_back: HTTPRequest = $PokeAPI/APIImageBack
 @onready var api_cry: HTTPRequest = $PokeAPI/APICry
 @onready var api_entry: HTTPRequest = $PokeAPI/APIEntry
 
 # SEARCH BAR STUFF GO HERE
 @export var search_bar:LineEdit
 @export var search_button:Button
-@onready var output: Label = $Output
 @onready var poke_cry: AudioStreamPlayer = $PokeCry
 
 # MINI DEX STUFF GO HERE
@@ -20,7 +20,10 @@ var mouse_offset = Vector2.ZERO
 @export var dex_anim:AnimationPlayer
 @export var search_bar_anim:AnimationPlayer
 @onready var dex_sprites:Node2D = $DemoDex/DexSprites
+@export var name_id_label:Label
 @export var poke_image: TextureRect
+var image_front:Texture2D
+var image_back:Texture2D
 @export var search_toggle:TextureButton
 var is_open:bool = false
 @export var closed_sprite:Sprite2D
@@ -29,6 +32,7 @@ var is_open:bool = false
 @export var icon_2:TextureRect
 @export var nav_right:TextureButton
 @export var nav_left:TextureButton
+@export var genera_label:Label
 var current_sprite:Sprite2D
 var is_hovering:bool = false
 var is_waiting:bool = true
@@ -43,6 +47,7 @@ var cry_audio:AudioStreamOggVorbis = AudioStreamOggVorbis.new()
 # NOTE: POKE ID IS REQUIRED FOR A LOTA STUFF, SO ITS DECLARED EARLIER HERE
 var p_name:String
 var p_id:int
+var p_genera:String
 var p_height:float
 var p_weight:float
 var p_type:Array[String]
@@ -66,10 +71,11 @@ var current_page:int = 0
 
 # API LINKS GO HERE
 var api_link:String = "https://pokeapi.co/api/v2/pokemon/"
-var image_link:String = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+var image_front_link:String = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+var image_back_link:String = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/'
 var cry_link:String = 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/'
 var cry_link_legacy:String = 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/'
-var entry_link:String = "https://pokeapi.co/api/v2/pokemon-species/"
+var species_link:String = "https://pokeapi.co/api/v2/pokemon-species/"
 var type_url:String = "res://type_icons/"
 
 # Connect a lota signals (mostly of APIs)
@@ -77,7 +83,8 @@ func _ready() -> void:
 	current_sprite = closed_sprite
 	api.request_completed.connect(_on_request_pokedata)
 	search_button.pressed.connect(search)
-	api_image.request_completed.connect(_on_request_pokeimage)
+	api_image_front.request_completed.connect(_on_request_pokeimage_front)
+	api_image_back.request_completed.connect(_on_request_pokeimage_back)
 	api_cry.request_completed.connect(_on_request_pokecry)
 	api_entry.request_completed.connect(_on_request_pokeentry)
 
@@ -138,7 +145,6 @@ func _on_request_pokedata(result:int, response_code:int, headers:PackedStringArr
 	p_sdefence = data["stats"][4]['base_stat'] # SPECIAL DEFENCE
 	p_speed = data["stats"][5]['base_stat'] # SPEED
 	
-	output.text = str(int(data["id"])) + ". " + str(data["name"]).to_upper()
 	print(int(data["id"]),'. ', data["name"])
 	print("Height: ", str(p_height), " Weight: ", str(p_weight))
 	print("Type: ", p_type)
@@ -152,6 +158,8 @@ func _on_request_pokedata(result:int, response_code:int, headers:PackedStringArr
 	print("S. DEF: ", p_sdefence)
 	print("SPD: ", p_speed)
 	
+	name_id_label.text = str(p_id) + '. ' + str(p_name
+	)
 	set_type()
 	search_image() # since now weve id, we can search the pokemon image
 	search_cry()
@@ -172,7 +180,8 @@ func set_type():
 		icon_2.texture = null
 
 func search_image() -> void:
-	var error = api_image.request(image_link + str(p_id) + '.png')
+	var error = api_image_front.request(image_front_link + str(p_id) + '.png')
+	error = api_image_back.request(image_back_link + str(p_id) + '.png')
 
 func search_cry() -> void:
 	var error
@@ -184,9 +193,9 @@ func search_cry() -> void:
 		print("Oops, there's an error when data was recieved!", str(error))
 
 func search_entry() -> void:
-	var error = api_entry.request(entry_link + str(p_id))
+	var error = api_entry.request(species_link + str(p_id))
 
-func _on_request_pokeimage(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
+func _on_request_pokeimage_front(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
 	if response_code != 200:
 		print("Failed to download image for ID: ", p_id)
 		return
@@ -195,8 +204,21 @@ func _on_request_pokeimage(result:int, response_code:int, headers:PackedStringAr
 	var error = image.load_png_from_buffer(body)
 	
 	if error == OK:
-		poke_image.texture = ImageTexture.create_from_image(image)
+		image_front = ImageTexture.create_from_image(image)
+		poke_image.texture = image_front
 		print("Pokemon image loaded successfully! ")
+
+func _on_request_pokeimage_back(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
+	if response_code != 200:
+		print("Failed to download image for ID: ", p_id)
+		return
+
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	if error == OK:
+		image_back = ImageTexture.create_from_image(image)
+		print("Pokemon's back sprite is succesfully loaded")
+
 
 func _on_request_pokecry(result:int, response_code:int, headers:PackedStringArray, body:PackedByteArray):
 	if response_code != 200:
@@ -217,6 +239,15 @@ func _on_request_pokeentry(result:int, response_code:int, headers:PackedStringAr
 	
 	var data:Dictionary = json.data
 	
+	# GENERA OF POKEMON
+	
+	for g in data["genera"]:
+		if g["language"]["name"] == SettingsGlobal.language:
+			p_genera = g["genus"]
+	
+	genera_label.text = p_genera
+	
+	# ENTRY FLAVOR TEXT
 	p_entry = ""
 	
 	for e in data["flavor_text_entries"]:
@@ -233,7 +264,6 @@ func _on_request_pokeentry(result:int, response_code:int, headers:PackedStringAr
 	while p_entry.length() > CHAR_LIMIT:
 		var split_at:int = p_entry.rfind(" ", CHAR_LIMIT)
 		
-		# Safety fallback if there somehow isn't a space
 		if split_at == -1:
 			split_at = CHAR_LIMIT
 		
@@ -278,7 +308,6 @@ func _input(event: InputEvent) -> void:
 				Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 				return
 			if current_sprite.get_rect().has_point(current_sprite.to_local(event.position)):
-				get_viewport().set_input_as_handled()
 
 				if not is_open:
 					if not is_mouse_on_search_toggle:
@@ -318,3 +347,11 @@ func _on_left_button_pressed() -> void:
 	if current_page > 0:
 		current_page -= 1
 		entry_text_label.text = entry_pages[current_page]
+
+
+func _on_flip_pressed() -> void:
+	print("SPRITE FLIPPED")
+	if poke_image.texture == image_front:
+		poke_image.texture = image_back
+	else:
+		poke_image.texture = image_front
